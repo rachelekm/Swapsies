d89c4ed39678da5fc8788d9b1e59aa10ad7b0e3c
Rachele Morino <rachelekm@gmail.com>
2018-07-16 21:39:41 -0400

    homepage component, redux initialized

---
+++ client/src/components/homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import Header from './header';
+
+export default function Game(props) {
+  return (
+    <div>
+      <Header />
+      <main role="main">
+      We're doing it!
+        //<GuessSection />
+        //<StatusSection />
+        //<InfoSection />
+      </main>
+    </div>
+  );
+}
